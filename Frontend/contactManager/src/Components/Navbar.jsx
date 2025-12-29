561a183f762f0e4b5f70231eaeeacf1c021300bf
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2025-12-29 10:35:06 +0530

    Adding Icon to title

--- Frontend/contactManager/src/Components/Navbar.jsx
+++ Frontend/contactManager/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-white shadow-sm">
-      <h1 className="text-lg font-semibold text-blue-600">📘 ContactBook</h1>
+      <h1 className="text-lg font-semibold text-blue-600">📘 Contact Book</h1>
       <div className=" flex justify-evenly gap-24">
         <Link to="/" className="text-gray-700 mt-1 text-xl hover:text-blue-600">Home</Link>
         <Link
