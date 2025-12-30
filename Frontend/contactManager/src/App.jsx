126caf9be7de09706c0bdc30a279a647509fe9b9
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2025-12-30 15:40:13 +0530

    minor edit

--- Frontend/contactManager/src/App.jsx
+++ Frontend/contactManager/src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import ContactList from "./pages/ContactList";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contacts" element={<ContactList />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
